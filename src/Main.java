eabb8a6269643e19ff6b1f1cc5a9539498b56347
gruckion <stephen.r.rayner@gmail.com>
2014-03-02 23:46:16 +0000

    Using ScheduledExecutorService

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,30 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Main implements Runnable {
-	
+
 	/*
 	 * main access point, that is via the Main's constructor.
-	 * */
+	 */
 	public static void main(String[] args) {
-		new Main();
+		System.out.println("Start");
+		 new Main();
 	}
-	
+
 	/*
 	 * The main constructor is the initialisation point for the program.
-	 * */
+	 */
 	public Main() {
 		System.out.println("Constructor call.");
-		startRunnable(this,1);
+		startRunnable(this, 1);
 	}
-	
+
 	/*
 	 * The runnable function for the main thread that process user input and
 	 * determines how and when the SingletonTask will be started.
-	 * */
+	 */
 	public void run() {
 		try {
-			//
-			while(true) {
-				System.out.println("Got here: " + count);
+			while (true) {
+				//System.out.println("Got here: " + count);
+				task.beepForAnHour();
 				count++;
 				try {
 					Thread.sleep(sleep);
@@ -36,22 +32,21 @@ public class Main implements Runnable {
 					ex.printStackTrace();
 				}
 			}
-			//
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return;
-	}
-
+	}	
+	
 	/*
 	 * standard process to producing a runnable thread
-	 * */
+	 */
 	public void startRunnable(Runnable runnable, int priority) {
 		Thread thread = new Thread(runnable);
 		thread.start();
 		thread.setPriority(priority);
 	}
-	
+
 	// The final instance task of the SigletonTask.
 	private final SingletonTask task = SingletonTask.getInstance();
 	// Count the loops through the Main thread.
--- src/SingletonTask.java
+++ src/SingletonTask.java
@@ -1,19 +1,57 @@
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.Executors;
 
-public class SingletonTask {
+import static java.util.concurrent.TimeUnit.SECONDS;
 
-	private static SingletonTask task = null;
+public class SingletonTask implements Runnable {
+
+	private static SingletonTask singleton = null;
 
 	protected SingletonTask() {
 		// protected singleton
 	}
 
 	public static SingletonTask getInstance() {
-		if(task == null)
-			task = new SingletonTask();
-		return task;
+		if(singleton == null)
+			singleton = new SingletonTask();
+		return singleton;
 	}
+	
+	/*
+	 * The runnable function for the SingletonTask thread that process
+	 * the task at hand.
+	 */
+	public void run() {
+		try {
+			System.out.println("2Got here: " + count);
+			count++;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return;
+	}	
 
-	// counter for the SingletonTask
-	private int count;
+	private final ScheduledExecutorService scheduler = Executors
+			.newScheduledThreadPool(1);
 
+	public void beepForAnHour() {
+		if(task == null)
+			task = this;
+		else 
+			return;
+		
+		final ScheduledFuture<?> beeperHandle = scheduler.scheduleAtFixedRate(
+				task, initalDelay, noramlDelay, SECONDS);
+		scheduler.schedule(new Runnable() {
+			public void run() {
+				beeperHandle.cancel(true);
+			}
+		}, duration, SECONDS);
+	}
+	
+	private Runnable task = null;;
+	private int initalDelay = 3, noramlDelay = 3;
+	private int duration = 3600;// Seconds
+	private int count = 0;// Counts the passes through the thread
 }
